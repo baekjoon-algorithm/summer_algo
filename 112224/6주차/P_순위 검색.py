70c4a68ce50877713bea33a5436092c49302864f
112224 <didgns25@naver.com>
2021-09-03 15:53:59 +0900

    6주차 문제 clear

--- 112224/6주차/P_순위 검색.py
+++ 112224/6주차/P_순위 검색.py
@@ -1,82 +1,51 @@
-# 혹시 해서 pandas 써봤더니 당연하게도 시간초과
-'''import pandas as pd
+from bisect import bisect_left
+def str2idx(s):
+    if s == '-':
+        return 0
+    if s in ['cpp', 'backend', 'junior', 'chicken']:
+        return 1
+    if s in ['java', 'frontend', 'senior', 'pizza']:
+        return 2
+    if s == 'python':
+        return 3
 
 
 def solution(info, query):
     answer = []
-    cols = ['lang', 'pre', 'year', 'food', 'score']
-    data = []
+
+    table = [[[[[] for _ in range(3)] for _ in range(3)] for _ in range(3)] for _ in range(4)]
+
     for ele in info:
         ele = ele.split()
-        data.append(ele)
-    df = pd.DataFrame(data, columns=cols)
+        score = int(ele[-1])
+        ele = [str2idx(s) for s in ele[:-1]]
+        ary = [[0,0,0,0] for _ in range(16)]
+        for i in range(1<<4):
+            for j in range(4):
+                if i & (1<<j):
+                    ary[i][j] = ele[j]
+        for lang, jobs, year, food in ary:
+            table[lang][jobs][year][food].append(score)
 
-    df[cols[4]] = df[cols[4]].apply(pd.to_numeric)
-    for ele in query:
-        ele = ele.split()
-        key = [ele[i] for i in range(0, 7, 2)] + [ele[7]]
+    for i in range(4):
+        for j in range(3):
+            for k in range(3):
+                for l in range(3):
+                    table[i][j][k][l].sort()
 
-        ret = df
-        condition =
-        for i in range(4):
-            if key[i] == '-':
-                continue
-            ret = ret[ret[cols[i]] == key[i]]
-        if key[4] != '-':
-            ret = ret[ret[cols[4]] >= int(key[4])]
-        answer.append(len(ret))
+    for qu in query:
+        qu = qu.split()
+        score = int(qu[-1])
+        op = [str2idx(s) for s in qu[:7:2]]
+        vi = table[op[0]][op[1]][op[2]][op[3]]
 
-    return answer'''
-
-
-def str2idx(s):
-    if s in ['cpp', 'backend', 'junior', 'chicken']:
-        return 0
-    if s in ['java', 'frontend', 'senior', 'pizza']:
-        return 1
-    if s == 'python':
-        return 2
-    return -1
+        n = len(vi)
+        k = bisect_left(vi, score)
+        answer.append(n - k)
 
 
-def solution(info, query):
-    answer = []
-    table = [dict() for _ in range(4)]
-    score = []
-    n = len(info)
-    for i in range(n):
-        ele = info[i]
-        ele = ele.split()
-        for j in range(4):
-            idx = str2idx(ele[j])
-            if idx not in table[j]:
-                table[j][idx] = set({i})
-            else:
-                table[j][idx].add(i)
-        score.append(int(ele[-1]))
-
-    whole = set()
-    whole.update(list(range(n)))
-    for ele in query:
-        ele = ele.split()
-        key = [ele[x] for x in range(0,7,2)]
-
-        ret = whole.copy()
-        for j in range(4):
-            idx = str2idx(key[j])
-            if idx == -1:
-                continue
-            if idx in table[j]:
-                ret &= table[j][idx]
-
-        cut = int(ele[7])
-        cnt = 0
-        for i in ret:
-            if score[i] >= cut:
-                cnt += 1
-        answer.append(cnt)
     return answer
 
 
 print(solution(["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"],
-               ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"]))
+    ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"]))
